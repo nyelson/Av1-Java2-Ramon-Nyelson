b8713a37bb8ce9d62eccb91b1fe5838b475f1052
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-07-20 22:12:54 -0300

    v0.3
    bla bla bla

---
+++ AV1_RamonPortela-NyelsonGomes/src/Carro.java
@@ -0,0 +1,66 @@
+
+public class Carro {
+	
+	private String chassi;
+	private String montadora;
+	private String modelo;
+	private String tipo;
+	private String cor;
+	private String cambio;
+	private float motorizacao;
+	private float preco;
+	
+	private void validarTipo(String tipo){			
+		}
+	}
+	
+	public String getChassi() {
+		return chassi;
+	}
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public String getMontadora() {
+		return montadora;
+	}
+	public void setMontadora(String montadora) {
+		this.montadora = montadora;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getCambio() {
+		return cambio;
+	}
+	public void setCambio(String cambio) {
+		this.cambio = cambio;
+	}
+	public float getMotorizacao() {
+		return motorizacao;
+	}
+	public void setMotorizacao(float motorizacao) {
+		this.motorizacao = motorizacao;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}	
+	
+}
